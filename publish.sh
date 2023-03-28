7b4513d799c2ca9118812e8dbad4314fac1a06b2
Alexander Schepanovski <suor.web@gmail.com>
2023-03-28 20:39:31 +0700

    Update publish script

--- publish.sh
+++ publish.sh
@@ -2,9 +2,10 @@
 
 set -ex
 
-NAME=funcy
-VERSION=`cat VERSION`
+NAME=pytest-matcher
+VERSION=`awk '/VERSION = /{gsub(/'\''/, "", $3); print $3}' pytest_matcher/__init__.py`
 
+echo "Publishing $NAME-$VERSION..."
 python setup.py sdist bdist_wheel
 twine check dist/$NAME-$VERSION*
 twine upload --skip-existing -uSuor dist/$NAME-$VERSION*
